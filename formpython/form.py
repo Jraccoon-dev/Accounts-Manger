7b6d16d433fb754c5a67a9ffecab93efb839069f
JackDelAguila <jackdelaguilavilla03@gmail.com>
2022-04-09 11:09:24 -0500

    add changes ui

--- formpython/form.py
+++ formpython/form.py
@@ -1,5 +1,5 @@
-from tkinter import *
-
+import tkinter as tk
+from tkinter import ttk, font
 import pyodbc
 
 server = 'MONSTER\SQLEXPRESS'
@@ -20,8 +20,6 @@ def send_data():
     email_data = email.get()
     password_data = str(password.get())
     context_data = context.get()
-    
-    # print (username_data,email_data,password_data)
 
     cursor = connectionServer.cursor()
     cursor.execute("Select * from accountManager")
@@ -32,50 +30,58 @@ def send_data():
     cursor.close()
     connectionServer.close()
     print("Succes")
-    merge_label = Label(text="Succes", bg="#f9f9f9")
+    merge_label = tk.Label(text="Succes", background="#f9f9f9",font=font.Font(size=16))
     merge_label.place(x=22,y=450)
 
-   
+#var global   
+spaceElement = 100
 
-
-window = Tk()
+window = tk.Tk()
 window.geometry("650x550")
 window.title("Accounts Management")
 window.resizable(False,False)
 window.config(background = "#f9f9f9")
-main_title = Label(text="Register form", font=("monospace", 13), bg="#f9f9f9",fg="#090909")
+main_title = tk.Label(text="Accounts Management", font=font.Font(size=18), bg="#f9f9f9",fg="#090909")
 main_title.pack()
 
-username_label = Label(text="Username", bg="#f9f9f9")
-username_label.place(x=22, y=70)
-
-email_label = Label(text="Email", bg="#f9f9f9")
-email_label.place(x=22, y=130)
-
-password_label = Label(text="Password", bg="#f9f9f9")
-password_label.place(x=22, y=190)
-
-context_label = Label(text="Context", bg="#f9f9f9")
-context_label.place(x=22, y=250)
+username_label = tk.Label(text="Username", bg="#f9f9f9",font=font.Font(size=14))
+username_label.place(x=22, y=80-3)
 
 
-username = StringVar()
-email = StringVar()
-password = StringVar()
-context = StringVar()
+email_label = tk.Label(text="Email", bg="#f9f9f9",font=font.Font(size=14))
+email_label.place(x=22, y=spaceElement+80-3)
 
-username_entry = Entry(textvariable=username, width="40")
-email_entry = Entry(textvariable=email, width="40")
-password_entry = Entry(textvariable=password, width="40", show="*")
-context_entry = Entry(textvariable=context,width="40")
+password_label = tk.Label(text="Password", bg="#f9f9f9",font=font.Font(size=14))
+password_label.place(x=22, y=spaceElement+180-3)
+
+context_label = tk.Label(text="Context", bg="#f9f9f9",font=font.Font(size=14))
+context_label.place(x=22, y=spaceElement+280-3)
+
+#Styles
+style = ttk.Style()
+style.configure(
+        "MyEntry.TEntry",
+        padding=10,
+        font=font.Font(family="monospace",size=13)
+        ) 
+
+username = tk.StringVar()
+email = tk.StringVar()
+password = tk.StringVar() 
+context = tk.StringVar()
+
+username_entry = ttk.Entry(textvariable=username,style="MyEntry.TEntry",width=40)
+email_entry = ttk.Entry(textvariable=email,style="MyEntry.TEntry",width=40)
+password_entry = ttk.Entry(textvariable=password,style="MyEntry.TEntry" , show="*",width=40)
+context_entry = ttk.Entry(textvariable=context,style="MyEntry.TEntry",width=40)
 
 username_entry.place(x=22, y=100)
-email_entry.place(x=22, y=160)
-password_entry.place(x=22, y=220)
-context_entry.place(x=22,y=280)
+email_entry.place(x=22, y=spaceElement*2)
+password_entry.place(x=22, y=spaceElement*3)
+context_entry.place(x=22,y=spaceElement*4)
 
-submit_btn = Button(window,text="Submit", command=send_data, width="30",height="2", bg="#f9f9f9")
-submit_btn.place(x=22, y=360)
+submit_btn = tk.Button(window,text="Submit",font=font.Font(size=12),fg="#f09800", command=send_data, width="24",height="2", bg="#f0f880", border=0)
+submit_btn.place(x=40, y=500)
 
 window.mainloop()
 
